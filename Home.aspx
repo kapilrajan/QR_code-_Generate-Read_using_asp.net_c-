683e2392134c998197051674171bfb9cc8d4bf05
ADMIN <ADMIN@DESKTOP-1NR65BN>
2023-08-01 12:26:57 +0530

    Add project files.

---
+++ Home.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="QR_code__Generate_Read_using_asp.netc_.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>QR_code _Generate&Read_using_asp.netc#</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+<asp:Button ID="btnGenerate" runat="server" Text="Generate QR Code" OnClick="QR_Generate_Click" />
+             <asp:Label ID="last"  runat="server" Font-Italic="True" Font-Size="Medium"></asp:Label>
+<hr />
+<asp:Image ID="imgQRCode" Width="100px" Height="100px" runat="server" Visible="false" />     <br /><br />
+<asp:Button ID="btnRead" Text="Read QR Image" runat="server" OnClick="QR_Read_Click" />  <br /><br />
+<asp:Label ID="lblQRCode" runat="server"></asp:Label>
+<br />
+
+<asp:Label ID="error" runat="server" Font-Italic="True" Font-Size="Medium"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZXing;
+
+namespace QR_code__Generate_Read_using_asp.netc_
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void QR_Generate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GenerateCode(txtCode.Text);
+            }
+            catch (Exception ex)
+            {
+                error.Text = ex.Message;
+            }
+            finally
+            {
+
+                last.Text = "Enter some text";
+
+            }
+        }
+
+        protected void QR_Read_Click(object sender, EventArgs e)
+        {
+            ReadQRCode();
+        }
+
+        private void GenerateCode(string name)
+        {
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            var result = writer.Write(name);
+            string path = Server.MapPath("~/images/kapilQRImage.jpg");
+            var barcodeBitmap = new Bitmap(result);
+
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+            imgQRCode.Visible = true;
+            imgQRCode.ImageUrl = "~/images/kapilQRImage.jpg";
+
+        }
+
+       
+        private void ReadQRCode()
+        {
+            var reader = new BarcodeReader();
+            string filename = Path.Combine(Request.MapPath("~/images"), "kapilQRImage.jpg");
+           
+            var result = reader.Decode(new Bitmap(filename));
+            if (result != null)
+            {
+                lblQRCode.Text = "QR Code: " + result.Text;
+            }
+        }
+
+
+    }
+}
